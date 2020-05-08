cfb769c14377d4a12e556f184223943130088699
Edvinas01 <edvinas108@gmail.com>
2020-05-08 23:14:40 +0200

    Fix scene loading and mixer setup

---
+++ Assets/Scripts/Audio/AudioMixerManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+namespace Audio
+{
+    public class AudioMixerManager : MonoBehaviour
+    {
+        [Tooltip("Audio mixer to be managed")]
+        public AudioMixer audioMixer;
+
+        [Tooltip("Snapshot transition duration in seconds")]
+        public float snapshotTransitionDuration = 5f;
+
+        public string fadedOutSnapshotName;
+        public string fadedInSnapshotName;
+
+        private AudioMixerSnapshot fadeOut;
+        private AudioMixerSnapshot fadeIn;
+
+        public void TransitionFadeOut()
+        {
+            fadeOut.TransitionTo(snapshotTransitionDuration);
+        }
+
+        public void TransitionFadeIn()
+        {
+            fadeIn.TransitionTo(snapshotTransitionDuration);
+        }
+
+        private void Awake()
+        {
+            fadeOut = audioMixer.FindSnapshot(fadedOutSnapshotName);
+            fadeIn = audioMixer.FindSnapshot(fadedInSnapshotName);
+        }
+    }
+}
--- Assets/Scripts/Util/SceneSwitcher.cs
+++ Assets/Scripts/Util/SceneSwitcher.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using Audio;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -9,35 +10,66 @@ namespace Util
         [Tooltip("Name of the scene which is to be loaded next")]
         public string nextSceneName;
 
+        [Tooltip("Audio mixer manager used for audio fading")]
+        public AudioMixerManager mixerManager;
+
         [Tooltip("Screen fade used to fade out the view on load")]
         public OVRScreenFade fade;
 
+        private Scene activeScene;
         private Scene nextScene;
 
+        private bool triggered;
+
         /// <summary>
-        /// Load scene with given name.
+        /// Reload active scene.
+        /// </summary>
+        public void ReloadActiveScene()
+        {
+            StartCoroutine(FadeOutLoad(activeScene.name));
+        }
+
+        /// <summary>
+        /// Load scene with name <see cref="nextSceneName"/>.
         /// </summary>
         public void LoadNextScene()
         {
-            StartCoroutine(FadeOutLoad());
+            StartCoroutine(FadeOutLoad(nextSceneName));
         }
 
-        private IEnumerator FadeOutLoad()
+        private IEnumerator FadeOutLoad(string sceneName)
         {
+            if (triggered)
+            {
+                yield break;
+            }
+
+            triggered = true;
+
+            mixerManager.TransitionFadeOut();
             yield return fade.CreateFadeOut();
-            SceneManager.LoadScene(nextSceneName);
+
+            SceneManager.LoadScene(sceneName);
         }
 
         private void Awake()
         {
+            activeScene = SceneManager.GetActiveScene();
             nextScene = SceneManager.GetSceneByName(nextSceneName);
+
             if (nextScene.IsValid())
             {
                 return;
             }
 
-            nextScene = SceneManager.GetActiveScene();
-            nextSceneName = nextScene.name;
+            // Use current scene as fallback.
+            nextSceneName = activeScene.name;
+            nextScene = activeScene;
+        }
+
+        private void Start()
+        {
+            mixerManager.TransitionFadeIn();
         }
     }
 }
