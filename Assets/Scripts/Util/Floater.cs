8c2aa1a6ee8eb0e7fafbac33c14078472978773d
Edvinas01 <edvinas108@gmail.com>
2020-05-15 23:28:01 +0200

    Add floaters

---
+++ Assets/Scripts/Util/Floater.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace Util
+{
+    public class Floater : MonoBehaviour
+    {
+        [Tooltip("The transform to float")]
+        public Transform floater;
+
+        [Tooltip("Which point to rotate around")]
+        public Transform rotateAround;
+
+        public Vector3 rotateAxis = Vector3.up;
+
+        public float floatIntensity = 1f;
+
+        public float rotateIntensity = 1f;
+
+        private Vector3 initialPosition;
+
+        private void Awake()
+        {
+            initialPosition = floater.position;
+        }
+
+        private void Update()
+        {
+            floater.RotateAround(
+                rotateAround.position,
+                rotateAxis,
+                Time.deltaTime * rotateIntensity
+            );
+
+            var yOffset = Mathf.Sin(Time.time) * floatIntensity;
+
+            var position = floater.position;
+
+            floater.position = new Vector3(
+                position.x,
+                initialPosition.y + yOffset,
+                position.z
+            );
+        }
+    }
+}
